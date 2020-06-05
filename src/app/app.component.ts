2a61d8a15fe9a420eaf9bbd718ad2854b5a39217
Soumya Bastikar <bastikarsoumya95@gmail.com>
2020-06-05 09:45:29 +0530

    IETesting Demo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.less']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'testIE';
+
+  itemReadOnly = false;
+
+
+  ngOnInit(): void {
+    // window.onblur = function() {
+    //   const inputEl = <HTMLInputElement>document.getElementById('inputName');
+    //   inputEl.readOnly = true;
+    //   inputEl.blur();
+    //   console.log('Window onBlur called'+inputEl.readOnly);
+    // };
+  }
+
+  // [readonly]="itemReadOnly"
+
+  onBlurHandler(event) {
+
+    const inputEl = <HTMLInputElement>document.getElementById('inputName');
+    inputEl.readOnly = true;
+    this.itemReadOnly = true;
+    console.log('readOnly'+inputEl.readOnly);
+    if(inputEl.readOnly){
+      // inputEl.blur();
+      console.log(event);
+      event.srcElement.readOnly = true;
+      console.log('blurred!');
+    }
+  }
+
+  onEdit(){
+    const inputEl = <HTMLInputElement>document.getElementById('inputName');
+    inputEl.readOnly = false;
+  }
+
+
+
 }
